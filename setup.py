9e1375e7a2cfae94b4a89bb5a2f1cfe29941c88f
abearab <abea@arcinstitute.org>
2024-07-12 16:58:57 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from __init__ import __version__, __author__, __email__
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    python_requires='>=3.9',
+    name='DeepGSEA',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="MIT License",
+
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    maintainer=__author__,
+    maintainer_email=__email__,
+
+    url='https://github.com/Teddy-XiongGZ/DeepGSEA',
+
+    packages=find_packages(include=['screenpro', 'screenpro.*']),
+    package_dir={'':'src'},
+
+    install_requires=required,
+
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ]
+)
---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+
+#TODO: Add import statements for all necessary modules in the package
+
+#TODO: Add the version, author, and email variables
+__version__ = "0.0.1"
+__author__ = "Guangzhi Xiong"
+__email__ = 'hhu4zu@virginia.edu'
